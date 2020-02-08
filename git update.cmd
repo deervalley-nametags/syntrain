f5c9dfb7a795005b4458cb955805f8d928870b1e
Em Thomas <emeqiss@deervalley.com>
2020-02-08 11:09:30 -0700

    123

---
+++ git update.cmd
@@ -0,0 +1,5 @@
+
+git add *
+git commit -m "comment"
+git push origin master
+pause
--- src/App.js
+++ src/App.js
@@ -1,25 +1,133 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { 
+  BrowserRouter as Router, 
+  Switch, 
+  Route, 
+  Link 
+} from 'react-router-dom';
 import './App.css';
+import './piano.css';
 
 function App() {
+  useEffect(() => {
+    const colorRelation = [ 1, 2 ];
+  }, []); //only run once
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Router>
+        
+        {/* -- TITLE -- */}
+        <Row>
+          Projekt Syntrain använder React
+        </Row>
+
+        {/* -- NAVBAR -- */}
+        <Row>
+          <Col>
+            <Link to="/">Hem</Link>
+          </Col>
+          <Col>
+            <Link to="/train">Träna</Link>
+          </Col>
+          <Col>
+            <Link to="/recall">Återkallande</Link>
+          </Col>
+        </Row>
+
+        {/* -- ROUTED PAGE -- */}
+        <Row>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/train">
+              <Train />
+            </Route>
+            <Route path="/recall">
+              <Recall />
+            </Route>
+          </Switch>
+        </Row>
+      </Router>
+    </Container>
+  );
+}
+
+function Home(){
+  return(
+    <Container>
+      <Row>
+        <Col>
+          {/* -- PIANO -- */}
+          <PianoKeyset />
+        </Col>
+      </Row>
+      home
+    </Container>
+  );
+}
+
+function PianoKeyset(){
+  const [ pianoColorArray, setPianoColorArray ] = useState([{
+    backgroundColor: "red"
+  },{
+    backgroundColor: "blue"
+  },{
+    backgroundColor: "green"
+  },{
+    backgroundColor: "green"
+  },{
+    backgroundColor: "green"
+  },{
+    backgroundColor: "green"
+  },{
+    backgroundColor: "green"
+  },{
+    backgroundColor: "green"
+  },{
+    backgroundColor: "green"
+  },{
+    backgroundColor: "green"
+  },{
+    backgroundColor: "green"
+  },{
+    backgroundColor: "green"
+  }]); //this whole build array will be gone when data import gets here
+
+  return(
+    <Container>
+      <div className="piano-c" style={pianoColorArray[3]} />
+      <div className="piano-d" />
+      <div className="piano-e" />
+      <div className="piano-f" />
+      <div className="piano-g" />
+      <div className="piano-a" />
+      <div className="piano-b" />
+      
+      <div className="piano-csharp" />
+      <div className="piano-dsharp" />
+      <div className="piano-fsharp" />
+      <div className="piano-gsharp" />
+      <div className="piano-asharp" />
+    </Container>
+  );
+}
+
+function Train(){
+  return(
+    <Container>
+      train
+    </Container>
+  );
+}
+
+function Recall(){
+  return(
+    <Container>
+      recall
+    </Container>
   );
 }
 
